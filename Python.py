e4e6befdc8dd0145804ea71b9fabe6440474e21d
Savvasun <savvazhukov2008@gmail.com>
2020-05-01 10:26:09 -0700

    Core Algorithm Commit

--- Python.py
+++ Python.py
@@ -1,1 +1,10 @@
+text = input("Type your message in english: ")
+answers = []
+words = text.split()
+answer = ""
 
+for i in range(len(words)):
+    ans = words[i]
+    answer += ans[2:] + ans[:2] + "ay" + " "
+
+print(answer)
