0217839b502c4ed91bb4529f69d62cf8ea472083
Savvasun <savvazhukov2008@gmail.com>
2020-05-02 16:46:14 -0700

    Config Commit

--- Python.py
+++ Python.py
@@ -1,9 +1,10 @@
 text = input("Type your message in english: ")
+num = int(input("How many of the first letters of the word do you want encrypted?: "))
 words = text.split()
 answer = ""
 
 for i in range(len(words)):
     ans = words[i]
-    answer += ans[2:] + ans[:2] + "ay" + " "
+    answer += ans[num:] + ans[:num] + "ay" + " "
 
 print(answer)
