d8d63fb3624c1fdceec65a3c15bd6a32b597633b
Savvasun <savvazhukov2008@gmail.com>
2020-05-04 10:56:17 -0700

    Text Prompt Commit

--- Python.py
+++ Python.py
@@ -1,10 +1,11 @@
 text = input("Type your message in english: ")
-num = int(input("How many of the first letters of the word do you want encrypted?: "))
+num = int(input("How many of the first letters of the word do you want moved?: "))
+textad = input("What text do you want ot be added at the end of each word?: ")
 words = text.split()
 answer = ""
 
 for i in range(len(words)):
     ans = words[i]
-    answer += ans[num:] + ans[:num] + "ay" + " "
+    answer += ans[num:] + ans[:num] + textad + " "
 
 print(answer)
