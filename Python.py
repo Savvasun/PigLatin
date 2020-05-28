ddfbac7785ba875a338990100a667885086bf28f
Savvasun <savvazhukov2008@gmail.com>
2020-05-28 13:47:51 -0700

    Customization Commit

--- Python.py
+++ Python.py
@@ -1,11 +1,25 @@
-text = input("Type your message in english: ")
-num = int(input("How many of the first letters of the word do you want moved?: "))
-textad = input("What text do you want ot be added at the end of each word?: ")
-words = text.split()
-answer = ""
+mode = input("Do you want classic Pig Latin (classic) or custom pig latin (custom)?: ")
 
-for i in range(len(words)):
-    ans = words[i]
-    answer += ans[num:] + ans[:num] + textad + " "
+if (mode == "custom"):
+    text = input("Type your message in english: ")
+    num = int(input("How many of the first letters of the word do you want moved?: "))
+    textad = input("What text do you want to be added at the end of each word?: ")
+    words = text.split()
+    answer = ""
 
-print(answer)
+    for i in range(len(words)):
+        ans = words[i]
+        answer += ans[num:] + ans[:num] + textad + " "
+
+    print(answer)
+
+if (mode == "classic"):
+    text = input("Type your message in english: ")
+    words = text.split()
+    answer = ""
+
+    for i in range(len(words)):
+        ans = words[i]
+        answer += ans[2:] + ans[:2] + "ay" + " "
+
+    print(answer)
