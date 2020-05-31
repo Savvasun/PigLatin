16507742a15c6647cb7c876d8e92083390e09635
Savvasun <savvazhukov2008@gmail.com>
2020-05-31 11:43:20 -0700

    Overhaul Commit

--- Python.py
+++ Python.py
@@ -1,25 +1,39 @@
-mode = input("Do you want classic Pig Latin (classic) or custom pig latin (custom)?: ")
+text = input("Type your message in english: ")
+words = text.split()
+mode = input("Do you want simple Pig Latin (simple), custom Pig Latin (custom), or classic Pig Latin? (classic): ")
+vow = "aeiou"
+cons = "bcdfghjklmnpqrstvxz"
+moveLet = 0
+answer = ""
 
 if (mode == "custom"):
-    text = input("Type your message in english: ")
     num = int(input("How many of the first letters of the word do you want moved?: "))
     textad = input("What text do you want to be added at the end of each word?: ")
-    words = text.split()
-    answer = ""
 
     for i in range(len(words)):
-        ans = words[i]
-        answer += ans[num:] + ans[:num] + textad + " "
+        word = words[i]
+        answer += word[num:] + word[:num] + textad + " "
+
+    print(answer)
+
+if (mode == "simple"):
+    for i in range(len(words)):
+        word = words[i]
+        answer += word[2:] + word[:2] + "ay" + " "
 
     print(answer)
 
 if (mode == "classic"):
-    text = input("Type your message in english: ")
-    words = text.split()
-    answer = ""
-
+    vowels = list(vow)
+    consonants = list(cons)
+    
     for i in range(len(words)):
-        ans = words[i]
-        answer += ans[2:] + ans[:2] + "ay" + " "
-
+        word = words[i]
+        
+        for i in range(len(list(word))):
+            if (any(x in word[i] for x in consonants) == True and any(x in word[i + 1] for x in vowels) == True):
+                moveLet = word.find(word[i]) - 1
+        
+        answer += word[moveLet:] + word[:moveLet] + "ay" + " "
+    
     print(answer)
